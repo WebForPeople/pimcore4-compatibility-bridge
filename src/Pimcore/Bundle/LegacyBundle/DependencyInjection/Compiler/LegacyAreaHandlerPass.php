60413969ba2de3e7d49ee86776e08fae2234ae93
Ales Seifert <seifert@alesak.net>
2020-03-08 01:12:14 +1100

    Fix for CompatibilityBridge error
    You have requested a non-existent service "pimcore.document.tag.handler"

--- src/Pimcore/Bundle/LegacyBundle/DependencyInjection/Compiler/LegacyAreaHandlerPass.php
+++ src/Pimcore/Bundle/LegacyBundle/DependencyInjection/Compiler/LegacyAreaHandlerPass.php
@@ -26,7 +26,7 @@ class LegacyAreaHandlerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         // add legacy handler to area handler
-        $handlerDefinition = $container->getDefinition('pimcore.document.tag.handler');
+        $handlerDefinition = $container->findDefinition('pimcore.document.tag.handler');
         $handlerDefinition->addMethodCall('addHandler', [new Reference('pimcore.document.tag.handler.legacy')]);
     }
 }
