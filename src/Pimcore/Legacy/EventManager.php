c2877db7e73878789f12b7167eae26a3ce4338b3
Ales Seifert <seifert@alesak.net>
2020-12-12 23:53:03 +1100

    Fixed bug in legacy events triggered multiple times (were triggered as many times as the same type of event were attached for different callbacks)

--- src/Pimcore/Legacy/EventManager.php
+++ src/Pimcore/Legacy/EventManager.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Pimcore
  *
@@ -31,8 +32,9 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 
 class EventManager extends \Zend_EventManager_EventManager {
 
-    public function attach($event, $callback = null, $priority = 1)
-    {
+    protected static $attachedEvents = [];
+
+    public function attach($event, $callback = null, $priority = 1) {
         // support multiple events passed as array - Zend_EventManager is expected
         // to return an array of listeners if an array of events is passed
         if (is_array($event)) {
@@ -55,126 +57,128 @@ class EventManager extends \Zend_EventManager_EventManager {
             "system.cache.clearOutputCache" => SystemEvents::CACHE_CLEAR_FULLPAGE_CACHE
         ];
 
-        if(isset($listenerMappings[$eventName])) {
+        if (isset($listenerMappings[$eventName])) {
             $newEventName = $listenerMappings[$eventName];
         }
 
-        \Pimcore::getEventDispatcher()->addListener($newEventName, function ($event) use ($self, $eventName) {
+        if (array_search($newEventName, self::$attachedEvents) === FALSE) {
+            \Pimcore::getEventDispatcher()->addListener($newEventName, function ($event) use ($self, $eventName) {
 
-            $target = null;
-            $params = [];
+                $target = null;
+                $params = [];
 
-            if($event instanceof ElementEventInterface) {
-                $target = $event->getElement();
-            }
-
-            if($event instanceof SearchBackendEvent) {
-                $target = $event->getData();
-            }
-
-            if($event instanceof UserRoleEvent) {
-                $target = $event->getUserRole();
-            }
-
-            if($event instanceof VersionEvent) {
-                $target = $event->getVersion();
-            }
-
-            if($event instanceof WorkflowEvent) {
-                $target = $event->getWorkflowManager();
-            }
-
-            if($event instanceof CollectionConfigEvent) {
-                $target = $event->getCollectionConfig();
-            }
-
-            if($event instanceof GroupConfigEvent) {
-                $target = $event->getGroupConfig();
-            }
-
-            if($event instanceof KeyConfigEvent) {
-                $target = $event->getKeyConfig();
-            }
-
-            if($event instanceof StoreConfigEvent) {
-                $target = $event->getStoreConfig();
-            }
-
-            if($event instanceof ClassDefinitionEvent) {
-                $target = $event->getClassDefinition();
-            }
-
-            if($event instanceof CustomLayoutEvent) {
-                $target = $event->getCustomLayout();
-            }
-
-            if($event instanceof ArgumentsAwareTrait) {
-                $params = $event->getArguments();
-            }
-
-            if($event instanceof GenericEvent) {
-                $target = $event->getSubject();
-                $params = $event->getArguments();
-            }
-
-            // add the Symfony event for debugging purposes
-            $params["__SYMFONY_EVENT"] = $event;
-
-            $returnValueContainerMappings = [
-                "admin.document.get.preSendData" => [
-                    "param" => "returnValueContainer",
-                    "argument" => "data"
-                ],
-                "admin.asset.get.preSendData" => [
-                    "param" => "returnValueContainer",
-                    "argument" => "data"
-                ],
-                "admin.class.objectbrickList.preSendData" => [
-                    "param" => "returnValueContainer",
-                    "argument" => "list"
-                ],
-                "admin.object.treeGetChildsById.preSendData" => [
-                    "param" => "returnValueContainer",
-                    "argument" => "objects"
-                ],
-                "admin.object.get.preSendData" => [
-                    "param" => "returnValueContainer",
-                    "argument" => "data"
-                ],
-                "admin.search.list.beforeFilterPrepare" => [
-                    "param" => "requestParams",
-                    "argument" => "requestParams"
-                ],
-                "admin.search.list.beforeListLoad" => [
-                    "param" => "list",
-                    "argument" => "data"
-                ],
-                "admin.search.list.afterListLoad" => [
-                    "param" => "list",
-                    "argument" => "data"
-                ],
-            ];
-
-            $isUsingReturnValueContainer = false;
-            $returnValueContainer = null;
-            if(isset($returnValueContainerMappings[$eventName]) && $event instanceof GenericEvent) {
-                if($event->hasArgument($returnValueContainerMappings[$eventName]["argument"])) {
-                    $dataFromArgument = $event->getArgument($returnValueContainerMappings[$eventName]["argument"]);
-                    $returnValueContainer = new \Pimcore\Model\Tool\Admin\EventDataContainer($dataFromArgument);
-                    $isUsingReturnValueContainer = true;
-
-                    $params[$returnValueContainerMappings[$eventName]["param"]] = $returnValueContainer;
+                if ($event instanceof ElementEventInterface) {
+                    $target = $event->getElement();
                 }
-            }
 
-            $self->trigger($eventName, $target, $params);
+                if ($event instanceof SearchBackendEvent) {
+                    $target = $event->getData();
+                }
 
-            if($isUsingReturnValueContainer) {
-                $event->setArgument($returnValueContainerMappings[$eventName]["argument"], $returnValueContainer->getData());
-            }
-        });
+                if ($event instanceof UserRoleEvent) {
+                    $target = $event->getUserRole();
+                }
 
+                if ($event instanceof VersionEvent) {
+                    $target = $event->getVersion();
+                }
 
+                if ($event instanceof WorkflowEvent) {
+                    $target = $event->getWorkflowManager();
+                }
+
+                if ($event instanceof CollectionConfigEvent) {
+                    $target = $event->getCollectionConfig();
+                }
+
+                if ($event instanceof GroupConfigEvent) {
+                    $target = $event->getGroupConfig();
+                }
+
+                if ($event instanceof KeyConfigEvent) {
+                    $target = $event->getKeyConfig();
+                }
+
+                if ($event instanceof StoreConfigEvent) {
+                    $target = $event->getStoreConfig();
+                }
+
+                if ($event instanceof ClassDefinitionEvent) {
+                    $target = $event->getClassDefinition();
+                }
+
+                if ($event instanceof CustomLayoutEvent) {
+                    $target = $event->getCustomLayout();
+                }
+
+                if ($event instanceof ArgumentsAwareTrait) {
+                    $params = $event->getArguments();
+                }
+
+                if ($event instanceof GenericEvent) {
+                    $target = $event->getSubject();
+                    $params = $event->getArguments();
+                }
+
+                // add the Symfony event for debugging purposes
+                $params["__SYMFONY_EVENT"] = $event;
+
+                $returnValueContainerMappings = [
+                    "admin.document.get.preSendData" => [
+                        "param" => "returnValueContainer",
+                        "argument" => "data"
+                    ],
+                    "admin.asset.get.preSendData" => [
+                        "param" => "returnValueContainer",
+                        "argument" => "data"
+                    ],
+                    "admin.class.objectbrickList.preSendData" => [
+                        "param" => "returnValueContainer",
+                        "argument" => "list"
+                    ],
+                    "admin.object.treeGetChildsById.preSendData" => [
+                        "param" => "returnValueContainer",
+                        "argument" => "objects"
+                    ],
+                    "admin.object.get.preSendData" => [
+                        "param" => "returnValueContainer",
+                        "argument" => "data"
+                    ],
+                    "admin.search.list.beforeFilterPrepare" => [
+                        "param" => "requestParams",
+                        "argument" => "requestParams"
+                    ],
+                    "admin.search.list.beforeListLoad" => [
+                        "param" => "list",
+                        "argument" => "data"
+                    ],
+                    "admin.search.list.afterListLoad" => [
+                        "param" => "list",
+                        "argument" => "data"
+                    ],
+                ];
+
+                $isUsingReturnValueContainer = false;
+                $returnValueContainer = null;
+                if (isset($returnValueContainerMappings[$eventName]) && $event instanceof GenericEvent) {
+                    if ($event->hasArgument($returnValueContainerMappings[$eventName]["argument"])) {
+                        $dataFromArgument = $event->getArgument($returnValueContainerMappings[$eventName]["argument"]);
+                        $returnValueContainer = new \Pimcore\Model\Tool\Admin\EventDataContainer($dataFromArgument);
+                        $isUsingReturnValueContainer = true;
+
+                        $params[$returnValueContainerMappings[$eventName]["param"]] = $returnValueContainer;
+                    }
+                }
+
+                $self->trigger($eventName, $target, $params);
+
+                if ($isUsingReturnValueContainer) {
+                    $event->setArgument($returnValueContainerMappings[$eventName]["argument"], $returnValueContainer->getData());
+                }
+            });
+            self::$attachedEvents[] = $newEventName;
+        }
         return parent::attach($event, $callback, $priority);
     }
+
 }
